993ce93cac669db2d48cc3bc2239b7c65bfab664
Binoy s s <83925422+Binoy-ss@users.noreply.github.com>
2022-01-19 22:55:00 +0530

    Q8

---
+++ Lab2/Q8.py
@@ -0,0 +1,19 @@
+#Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+usertuple = tuple(input("enter the items to be stored inside a tuple : "))
+
+if len(usertuple) > 4:
+
+    print(
+        "The elements in your tuple :",
+        usertuple,
+        "\n",
+        "The forth element in your tuple is : ",
+        usertuple[4],
+        "\n",
+        "The forth element from the last is :",
+        usertuple[-4],
+    )
+
+else:
+    print("Please enter at least 4 elements")
