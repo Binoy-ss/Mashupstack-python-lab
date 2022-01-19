89164c9911adbe280ffdae1511cb0837d2f70396
Binoy s s <83925422+Binoy-ss@users.noreply.github.com>
2022-01-19 22:14:11 +0530

    Add files via upload

---
+++ Lab2/Q3.py
@@ -0,0 +1,12 @@
+#Write a Python program to calculate the length of a string.
+
+userinput = input ("Enter string:")
+
+
+i = userinput.replace(" ", "")
+
+print( len(i), "string in your input")
+
+
+
+
