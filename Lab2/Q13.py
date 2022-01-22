083414b633af99452da4c1ad6114106cb3e40eaa
Binoy s s <83925422+Binoy-ss@users.noreply.github.com>
2022-01-22 17:25:52 +0530

    Add files via upload

---
+++ Lab2/Q13.py
@@ -0,0 +1,11 @@
+#Write a Python program to generate all sublists of a list.
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+b = [[]]
+
+for i in range(len(a) + 1):
+    for j in range(i):
+        b.append(a[j:i])
+
+print("The sublist of the given list ",b)
+
